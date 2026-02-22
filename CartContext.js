04622affc5531ec88cfd741c344d125dd749b86d
Vikash-malakar <vikashmalakar312@gmail.com>
2026-02-22 08:04:02 +0530

    react with CartContext learing

---
+++ CartContext.js
@@ -0,0 +1,68 @@
+import React, { createContext, useReducer } from "react";
+
+export const CartContext = createContext();
+
+const initialState = {
+  cart: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      const existingItem = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingItem) {
+        return {
+          cart: state.cart.map((item) =>
+            item.id === action.payload.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }
+
+      return {
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+      };
+
+    case "INCREASE":
+      return {
+        cart: state.cart.map((item) =>
+          item.id === action.payload
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      };
+
+    case "DECREASE":
+      return {
+        cart: state.cart
+          .map((item) =>
+            item.id === action.payload
+              ? { ...item, quantity: item.quantity - 1 }
+              : item
+          )
+          .filter((item) => item.quantity > 0),
+      };
+
+    case "REMOVE":
+      return {
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+}
+
+export function CartProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
