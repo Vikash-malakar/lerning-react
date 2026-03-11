fe5cac33a3cd15b528fdb5439d388d64069754ee
Vikash-malakar <vikashmalakar312@gmail.com>
2026-03-11 19:26:08 +0530

    learning react toast

--- App.jsx
+++ App.jsx
@@ -1,42 +1,33 @@
-import React, { useState } from "react";
+import {toast} from 'react-toastify'
 
-export default function App() {
-  const [birthYear, setBirthYear] = useState("");
-  const [age, setAge] = useState(null);
 
-  const calculateAge = () => {
-    const currentYear = new Date().getFullYear();
-    setAge(currentYear - birthYear);
-  };
+function App(){
+  function demo(){
+     toast("successfulllly ",{
+      type:"success",
+      position:"top-center",
+      autoClose:2000,
+      hideProgressBar:false,
+      closeOnClick:true,
+      pauseOnHover:true,
+      draggable:true,
+      progress:undefined,
+      theme:"light",
+      
+      
 
-  return (
-    <div
-      style={{
-        textAlign: "center",
-        marginTop: "100px",
-        fontFamily: "Arial",
-      }}
-    >
-      <h1>Age Calculator 🎂</h1>
+      
+    
 
-      <input
-        type="number"
-        placeholder="Enter your birth year"
-        value={birthYear}
-        onChange={(e) => setBirthYear(e.target.value)}
-        style={{ padding: "8px", fontSize: "16px" }}
-      />
-
-      <br /><br />
-
-      <button
-        onClick={calculateAge}
-        style={{ padding: "10px 20px", fontSize: "16px" }}
-      >
-        Calculate Age
-      </button>
-
-      {age && <h2>Your Age: {age}</h2>}
-    </div>
-  );
+     })
 }
+ 
+  return (
+    <>
+    <button onClick={demo}>click here </button>
+      
+    </>
+  )
+
+}
+export default App
