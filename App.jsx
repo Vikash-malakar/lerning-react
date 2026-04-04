49f75e9ee5b572d75d4429acb2d991a92b656838
Vikash-malakar <vikashmalakar312@gmail.com>
2026-04-04 20:52:21 +0530

    react learnig

--- App.jsx
+++ App.jsx
@@ -1,53 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const data = {
-    fruits: ["Apple", "Banana", "Mango"],
-    animals: ["Dog", "Cat", "Lion"],
-    colors: ["Red", "Green", "Blue"]
-  };
+  const [key, setKey] = useState("");
 
-  const [activeTab, setActiveTab] = useState("fruits");
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      setKey(e.key);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <div
       style={{
         textAlign: "center",
-        marginTop: "80px",
+        marginTop: "100px",
         fontFamily: "Arial"
       }}
     >
-      <h1>Dynamic Tabs App</h1>
+      <h1>Keyboard Key Detector</h1>
 
-      {/* Tabs */}
-      <div>
-        <button onClick={() => setActiveTab("fruits")} style={{ margin: "5px" }}>
-          Fruits
-        </button>
-        <button onClick={() => setActiveTab("animals")} style={{ margin: "5px" }}>
-          Animals
-        </button>
-        <button onClick={() => setActiveTab("colors")} style={{ margin: "5px" }}>
-          Colors
-        </button>
-      </div>
-
-      {/* Content */}
-      <div style={{ marginTop: "20px" }}>
-        {data[activeTab].map((item, index) => (
-          <div
-            key={index}
-            style={{
-              border: "1px solid #ccc",
-              padding: "10px",
-              margin: "5px auto",
-              width: "200px"
-            }}
-          >
-            {item}
-          </div>
-        ))}
-      </div>
+      <h2>Pressed Key: {key}</h2>
     </div>
   );
 }
